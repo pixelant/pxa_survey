fd57d72cd07622fb0bf660c8a8a33609d7d583a0
Andriy Oprysko <andriy@pixelant.se>
2017-11-16 12:07:34 +0200

    [TASK] added readme, small fixes

--- Configuration/TypoScript/PageTS/wizards.ts
+++ Configuration/TypoScript/PageTS/wizards.ts
@@ -2,7 +2,7 @@ mod {
     wizards.newContentElement.wizardItems.plugins {
         elements {
             pxa_survey {
-                #iconIdentifier = ext-pxa-survey-wizard-icon
+                iconIdentifier = ext-pxa-survey-wizard-icon
                 title = LLL:EXT:pxa_survey/Resources/Private/Language/locallang_be.xlf:plugin.name
                 description = LLL:EXT:pxa_survey/Resources/Private/Language/locallang_be.xlf:plugin.description
                 tt_content_defValues {
