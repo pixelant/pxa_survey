6bbc2fb3e0e73c4cc6da7a6994651eb6e91a56f4
Andriy Oprysko <andriy@pixelant.se>
2017-11-16 13:12:42 +0200

    [TASK] change requiremenets for TYPO3 7

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,8 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-8.7.99',
+            'typo3' => '7.6.0-8.7.99',
+            'php' => '7.0.0-7.1.99'
         ],
         'conflicts' => [],
         'suggests' => [],
