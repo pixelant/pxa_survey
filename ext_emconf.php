ce91fb4809b6dd715a6bbf0cdb679f241e1f6e04
h-e-l-l-o-w-o-r-l-d <51790093+h-e-l-l-o-w-o-r-l-d@users.noreply.github.com>
2021-01-26 13:00:57 +0100

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,8 +21,8 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.4.2',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-9.5.99',
-            'php' => '7.0.0-7.2.99'
+            'typo3' => '8.7.0-10.4.99',
+            'php' => '7.0.0-7.4.99'
         ],
         'conflicts' => [],
         'suggests' => [],
